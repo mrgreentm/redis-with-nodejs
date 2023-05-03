3758bd152f45f1aafa8b48d9a8ff04097b77c141
joao jacinto <joaonetotfm@gmail.com>
2023-05-02 22:14:44 -0300

    feature: finaliza projeto

--- index.js
+++ index.js
@@ -2,30 +2,62 @@ import Redis from "ioredis";
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
 // inicializa uma instância do redis
 const client = new Redis();
 
-function inserir(chave, valor) {
+function returnKeyParam(params) {
+  return params.key;
+}
+
+app.get("/getByKey/:key", async (req, res) => {
+  const key = returnKeyParam(req.params);
   try {
-    client.set(chave, valor);
+    const result = await client.get(key);
+    res.send(result);
   } catch (error) {
     throw error;
   }
-}
+});
+
+app.post("/setByKey/:key", async (req, res) => {
+  const body = req.body;
+  const key = returnKeyParam(req.params);
+  try {
+    client.set(key, body.value);
+    res.send("Valor inserido com sucesso!");
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.put("/putByKey/:key", async (req, res) => {
+  const body = req.body;
+  const key = returnKeyParam(req.params);
+  try {
+    client.set(key, body.value);
+    res.send("Valor alterado com sucesso!");
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.delete("/deleteByKey/:key", async (req, res) => {
+  const key = returnKeyParam(req.params);
+  try {
+    client.del([key]);
+    res.send("Valor deletado com sucesso!");
+  } catch (error) {
+    throw error;
+  }
+});
 
 client.connect(() => {
   console.log("Conectando ao Servidor Redis!!!!!!!!!");
-
-  inserir("aluno", "joao");
 });
 // caso haja algum erro de conexão com o servidor redis
 client.on("error", (error) => {
   console.error("Erro na conexão com Redis", error);
-});
-
-app.get("/getByKey/:key", async (req, res) => {
-  const key = req.params.key
-  const result = await client.get(key);
-  console.log(`${key}: `, result);
 });
 app.listen(3000);
